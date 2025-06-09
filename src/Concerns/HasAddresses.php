0e9b73dc0d44dbf0d7daac982dc0f19f271f3006
Victor R <39545521+viicslen@users.noreply.github.com>
2025-06-09 15:21:29 -0400

    feat: add trait to be used in other models

---
+++ src/Concerns/HasAddresses.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace ViicSlen\Addressable\Concerns\AddressBook;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
+use ViicSlen\Addressable\Models\AddressBook\Address;
+
+/**
+ * @used-by \Illuminate\Database\Eloquent\Model
+ * @mixin \Illuminate\Database\Eloquent\Model
+ */
+trait HasAddresses
+{
+    public function addresses(): MorphMany
+    {
+        return $this->morphMany(Address::class, 'addressable');
+    }
+
+    public function billingAddresses(): MorphMany
+    {
+        return $this->addresses()->where('billing', true);
+}
+
+    public function shippingAddresses(): MorphMany
+    {
+        return $this->addresses()->where('shipping', true);
+    }
+
+    public function primaryAddress(): MorphOne
+    {
+        return $this->addresses()->one()->ofMany($this->getPrimaryAggregateColumns(), fn (Builder $query) => $query->where('primary', true));
+    }
+
+    public function primaryBillingAddress(): MorphOne
+    {
+        return $this->billingAddresses()->one()->ofMany($this->getPrimaryAggregateColumns(), fn (Builder $query) => $query->where('primary', true));
+    }
+
+    public function primaryShippingAddress(): MorphOne
+    {
+        return $this->shippingAddresses()->one()->ofMany($this->getPrimaryAggregateColumns(), fn (Builder $query) => $query->where('primary', true));
+    }
+
+    protected function getPrimaryAggregateColumns(): array
+    {
+        return ['created_at' => 'max'];
+    }
+}
