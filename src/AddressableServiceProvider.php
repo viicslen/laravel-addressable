81583d51d672ef253414d135f78590941514a33a
Victor R <39545521+viicslen@users.noreply.github.com>
2025-06-27 11:30:09 -0400

    fix: register custom validation rules for country and currency codes in AddressableServiceProvider

--- src/AddressableServiceProvider.php
+++ src/AddressableServiceProvider.php
@@ -3,6 +3,8 @@
 namespace ViicSlen\Addressable;
 
 use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\InvokableValidationRule;
 use Sokil\IsoCodes\IsoCodesFactory;
 use Sokil\IsoCodes\TranslationDriver\SymfonyTranslationDriver;
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
@@ -13,6 +15,8 @@ use ViicSlen\Addressable\Events\AddressSaved;
 use ViicSlen\Addressable\Exceptions\InvalidAddressValidator;
 use ViicSlen\Addressable\Listeners\ValidateAddress;
 use ViicSlen\Addressable\Listeners\ValidateAddressQueued;
+use ViicSlen\Addressable\Rules\CountryCode;
+use ViicSlen\Addressable\Rules\CurrencyCode;
 
 class AddressableServiceProvider extends PackageServiceProvider
 {
@@ -30,18 +34,44 @@ class AddressableServiceProvider extends PackageServiceProvider
             });
     }
 
+    public function packageBooted(): void
+    {
+        if (! config('addressable.validation.enabled', true)) {
+            return;
+        }
+
+        $listener = config('addressable.validation.queued', true)
+            ? ValidateAddressQueued::class
+            : ValidateAddress::class;
+
+        Event::listen(AddressSaved::class, $listener);
+    }
+
     public function packageRegistered(): void
     {
-        $this->app->singleton(IsoCodesFactory::class, function () {
-            $driver = new SymfonyTranslationDriver;
-            $driver->setLocale(config('app.locale', 'en_US'));
+        $this->registerIsoCodeFactory();
+        $this->registerValidationRules();
+        $this->registerAddressValidator();
 
-            return new IsoCodesFactory(
-                null,
-                $driver
-            );
+    }
+
+    protected function registerValidationRules(): void
+    {
+        Validator::extend('country', static function ($attribute, $value, $parameters, $validator) {
+            return InvokableValidationRule::make(new CountryCode())
+                ->setValidator($validator)
+                ->passes($attribute, $value);
         });
 
+        Validator::extend('currency', static function ($attribute, $value, $parameters, $validator) {
+            return InvokableValidationRule::make(new CurrencyCode())
+                ->setValidator($validator)
+                ->passes($attribute, $value);
+        });
+    }
+
+    protected function registerAddressValidator(): void
+    {
         /** @var \ViicSlen\Addressable\Contracts\ValidatesAddress|null $validator */
         $validator = config('addressable.validation.validator');
 
@@ -56,16 +86,13 @@ class AddressableServiceProvider extends PackageServiceProvider
         $this->app->bind(ValidatesAddress::class, $validator);
     }
 
-    public function packageBooted(): void
+    protected function registerIsoCodeFactory(): void
     {
-        if (! config('addressable.validation.enabled', true)) {
-            return;
-        }
+        $this->app->singleton(IsoCodesFactory::class, function () {
+            $driver = new SymfonyTranslationDriver;
+            $driver->setLocale(config('app.locale', 'en_US'));
 
-        $listener = config('addressable.validation.queued', true)
-            ? ValidateAddressQueued::class
-            : ValidateAddress::class;
-
-        Event::listen(AddressSaved::class, $listener);
+            return new IsoCodesFactory(translationDriver: $driver);
+        });
     }
 }
